716f6baf34efb855d899335649c39623cd85164f
Milimas <4929474+Milimas@users.noreply.github.com>
2024-03-27 07:12:16 +0000

    playground

--- main.js
+++ main.js
@@ -1,76 +1,95 @@
-import * as THREE from 'three';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import * as THREE from 'three'
+import { LoadGLTFByPath } from './ModelHelper.js'
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-renderer.setClearColor( 0x404040, 1 );
-document.body.appendChild( renderer.domElement );
-
-const loader = new GLTFLoader();
-let paddle1 = new THREE.Object3D();
-
-loader.load( './public/pingpong_paddle.glb', function ( gltf ) {
-
-    gltf.scene.position.set(0, 0, 0);
-    gltf.scene.scale.set(0.5, 0.5, 0.5);
-    gltf.scene.rotation.set(0, 0, 0);
-    
-    paddle1.add(gltf.scene);
-}, undefined, function (error) {
-
-    console.error(error);
-
+//Renderer does the job of rendering the graphics
+let renderer = new THREE.WebGLRenderer({
+    //Defines the canvas component in the DOM that will be used
+	canvas: document.querySelector('#background'),
+  antialias: true,
 });
 
-scene.add(paddle1);
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+//set up the renderer with the default settings for threejs.org/editor - revision r153
+renderer.shadows = true;
+renderer.shadowType = 1;
+renderer.shadowMap.enabled = true;
+renderer.setPixelRatio( window.devicePixelRatio );
+renderer.toneMapping = 0;
+renderer.toneMappingExposure = 1
+renderer.toneMapping = THREE.NoToneMapping;
+renderer.setClearColor(0xffffff, 0);
+//make sure three/build/three.module.js is over r152 or this feature is not available. 
+renderer.outputColorSpace = THREE.SRGBColorSpace 
+
+const scene = new THREE.Scene();
+
+let cameraList = [];
+
+let camera;
+let player;
+
+// Load the GLTF model
+LoadGLTFByPath(scene)
+  .then(() => {
+    retrieveListOfCameras(scene);
+  })
+  .catch((error) => {
+    console.error('Error loading JSON scene:', error);
+  });
+
+//retrieve list of all cameras
+function retrieveListOfCameras(scene){
+  // Get a list of all cameras in the scene
+  scene.traverse(function (object) {
+    if (object.isCamera) {
+      cameraList.push(object);
+    }
+    if (object.name === "PaddlePlayer1")
+      player = object ;
+  });
+
+  //Set the camera to the first value in the list of cameras
+  camera = cameraList[0];
+
+  updateCameraAspect(camera);
+
+  // Start the animation loop after the model and cameras are loaded
+  animate();
+}
+
+// Set the camera aspect ratio to match the browser window dimensions
+function updateCameraAspect(camera) {
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+  camera.aspect = width / height;
+  camera.updateProjectionMatrix();
+}
+
+//A method to be run each time a frame is generated
+function animate() {
+  requestAnimationFrame(animate);
+
+  renderer.render(scene, camera);
+};
+
+
+const ambient = new THREE.AmbientLight() ;
+
+scene.add(ambient) ;
 
 var plane = new THREE.Plane(new THREE.Vector3(0, 0, 1), -2);
 var raycaster = new THREE.Raycaster();
 var mouse = new THREE.Vector2();
 var pointOfIntersection = new THREE.Vector3();
 
-function onDocumentMouseMove (event) {
-
-    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-    mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
-    raycaster.setFromCamera(mouse, camera);
-    raycaster.ray.intersectPlane(plane, pointOfIntersection);
-    paddle1.position.copy(pointOfIntersection);
-
+function onDocumentMouseMove(event)
+{
+  mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+  mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+  raycaster.setFromCamera(mouse, camera);
+  raycaster.ray.intersectPlane(plane, pointOfIntersection);
+  player.position.x = pointOfIntersection.x;
 }
 
-document.addEventListener('mousemove', onDocumentMouseMove) ;
-
-
-
-// add ambient light
-const ambientLight = new THREE.AmbientLight(0x404040);
-// make is strong
-ambientLight.intensity = 6;
-scene.add(ambientLight);
-
-// add a directional light
-const light = new THREE.DirectionalLight(0xffffff, 0.5);
-light.position.setScalar(10);
-scene.add(light);
-
-camera.position.z = 10;
-
-// on window resize
-window.addEventListener('resize', () => {
-    // update sizes
-    camera.aspect = window.innerWidth / window.innerHeight
-    camera.updateProjectionMatrix()
-    renderer.setSize(window.innerWidth, window.innerHeight)
-})
-
-function animate() {
-	requestAnimationFrame( animate );
-    
-	renderer.render( scene, camera );
-}
-animate();
-
+document.addEventListener('mousemove', onDocumentMouseMove, false);
